ae14e55ba2508d2aa38b7592a1fe04bac8ecbd32
OlekVano <olekvano@gmail.com>
2023-01-18 22:37:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
+const corsOptions = {
+  //origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.get('/test', cors(corsOptions), (req, res) => {
+  res.send('asdf')
+})
+
+const port = process.env.PORT || 3001
+app.listen(port, async () => {
+  console.log( `Server started at http://localhost:${ port }` );
+})
