ff5482bced99e7f5a2dc5a90265412c0ca37fdda
OlekVano <olekvano@gmail.com>
2023-01-19 22:30:34 +0000

    Code refactoring in users.js

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,19 @@
 import express from 'express'
-import { users } from '../firebase-setup.js'
+import { isOwner, getUserById } from '../utils.js'
+import { userKeys } from '../consts.js'
+import { getToken, users, app } from '../firebase-setup.js'
 const router = express.Router()
 
 router.get('/:userId', async (req, res) => {
   const { userId } = req.params
   try {
-    const snapshot = await users.where('id', '==', userId).get();
-    if (snapshot.empty) {
-      res.status(404).send('Not found')
-      return;
+    const user = await getUserById(userId)
+    console.log(user)
+    if (!user) {
+      res.status(404).send()
+      return
     }
-    const result = []
-    snapshot.forEach(doc => {
-      result.push(doc.data())
-    });
-    
-    res.json(result[0])
-
+    else res.json(user)
   } catch (err) {
     console.log(err)
     res.status(500).json({ message: err })
