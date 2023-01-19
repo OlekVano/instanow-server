7f86d0fbd9ab720ce1583032a036a315643d5899
OlekVano <olekvano@gmail.com>
2023-01-19 22:29:48 +0000

    Added isOwner function to the utils.js

--- utils.js
+++ utils.js
@@ -1,6 +1,25 @@
-import { verifyToken } from './firebase-setup.js'
+import { getToken, verifyToken, users, app } from './firebase-setup.js'
 
 export async function isAuthenticated(req, res, next) {
-  if (!await verifyToken(req)) res.status(401).send()
+  if (!await verifyToken(req)) res.status(403).send()
   else next()
+}
+
+export async function isOwner(req, userId) {
+  const token = getToken(req)
+  if (!token) return false
+  const payload = await app.auth().verifyIdToken(token)
+  return payload.uid == userId
+}
+
+export async function getUserById(id) {
+  const snapshot = await users.where('id', '==', id).get();
+  if (snapshot.empty) return undefined
+
+  const result = []
+  snapshot.forEach(doc => {
+    result.push(doc.data())
+  });
+    
+  return result[0]
 }
