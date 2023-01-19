05086354381751e5e757be67136a05e24cc883c3
OlekVano <olekvano@gmail.com>
2023-01-19 11:05:01 +0000

    Renamed firebase-functions.js to firebase-setup.js

--- firebase-setup.js
+++ firebase-setup.js
@@ -2,8 +2,12 @@ import admin from 'firebase-admin'
 import serviceAccount from './minecodia-firebase-adminsdk.json' assert {type: 'json'}
 
 const app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://minecodia.firebaseio.com'
 })
+
+const db = admin.firestore()
+export const users = db.collection('users')
 
 function getToken(req) {
   if (!req.headers.authorization) return undefined
