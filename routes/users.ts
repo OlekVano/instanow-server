c2326ff519a67e8e68bdbb08be819ebdd9827d9f
OlekVano <olekvano@gmail.com>
2023-01-20 00:12:53 +0000

    Removed unnecessary console.logs and improved code quality

--- routes/users.ts
+++ routes/users.ts
@@ -21,27 +21,33 @@ router.get('/:userId', async (req: Request<{userId: string}>, res: Response) => 
 })
 
 router.post('/:userId', async (req: Request<{userId: string}>, res: Response) => {
-  const { userId } = req.params
-  if (!await isOwner(req, userId)) res.status(403).send()
-
-  const keys = Object.keys(req.body)
-
-  if (!keys.every(key => (userKeys as string[]).includes(key))) {
-    res.status(400).send()
-    return
+  try {
+    const { userId } = req.params
+    if (!await isOwner(req, userId)) res.status(403).send()
+  
+    const keys = Object.keys(req.body)
+  
+    if (!keys.every(key => (userKeys as string[]).includes(key))) {
+      res.status(400).send()
+      return
+    }
+  
+    const token = getToken(req)
+  
+    if (!token) {
+      res.status(400).send()
+      return
+    }
+  
+    const payload = await app.auth().verifyIdToken(token)
+    await users.doc(payload.uid).set(req.body, { merge: true })
+  
+    res.status(200).send()
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: err })
   }
-
-  const token = getToken(req)
-
-  if (!token) {
-    res.status(400).send()
-    return
-  }
-
-  const payload = await app.auth().verifyIdToken(token)
-  await users.doc(payload.uid).set(req.body, { merge: true })
-
-  res.status(200).send()
+  
 })
 
 export default router
--- utils.ts
+++ utils.ts
@@ -10,18 +10,15 @@ export async function isAuthenticated(req: Request, res: Response, next: Functio
 export async function isOwner(req: Request, userId: string): Promise<boolean> {
   const token: string | undefined = getToken(req)
   if (!token) return false
+  
   const payload = await app.auth().verifyIdToken(token)
-  console.log('isOwner')
-  console.log(payload)
-  console.log(payload.uid)
   return payload.uid == userId
 }
 
 export async function getUserById(id: string): Promise<User | undefined> {
   const doc = await users.doc(id).get()
-  console.log(doc)
   if (!doc.exists) return undefined
+
   const data = doc.data()
-  console.log(data)
   return data as User
 }
