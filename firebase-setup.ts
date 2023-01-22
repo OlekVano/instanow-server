c01fa6a5613105a8d53e9c19796e558426450dbc
OlekVano <olekvano@gmail.com>
2023-01-22 19:36:00 +0000

    Added storage bucket to firebase-setup.ts

--- firebase-setup.ts
+++ firebase-setup.ts
@@ -1,14 +1,19 @@
 import { Request } from 'express'
 import admin, { ServiceAccount } from 'firebase-admin'
+import { getStorage } from 'firebase-admin/storage'
 import serviceAccount from './minecodia-firebase-adminsdk.json'
 
 export const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as ServiceAccount),
-  databaseURL: 'https://minecodia.firebaseio.com'
+  databaseURL: 'https://minecodia.firebaseio.com',
+  storageBucket: 'minecodia.appspot.com'
 })
 
 const db = app.firestore()
 export const users = db.collection('users')
+
+const storage = getStorage()
+export const bucket = storage.bucket();
 
 export function getToken(req: Request): string | undefined {
   if (!req.headers.authorization) return undefined
