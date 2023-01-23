7d741fceeec0e7ba314c0b167a128c417e20cbbf
OlekVano <olekvano@gmail.com>
2023-01-23 23:37:23 +0000

    Updated the userKeys const in consts.ts

--- consts.ts
+++ consts.ts
@@ -2,7 +2,9 @@ import { User } from './types'
 
 export const userKeys: (keyof User)[] = [
     'nickname',
-    'description'
+    'description',
+    'skin',
+    'background'
 ]
 //https://firebasestorage.googleapis.com/v0/b/{**BUCKET**}/o/{**PATH**}?alt=media
 export const storageBucketPath = 'https://firebasestorage.googleapis.com/v0/b/minecodia.appspot.com'
