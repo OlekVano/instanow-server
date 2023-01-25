752f05c97bdbdf4835849224a9f3f8e49c79c004
OlekVano <olekvano@gmail.com>
2023-01-25 23:11:52 +0000

    Increased the request size limit

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express'
 const app = express()
 import cors from 'cors'
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit: '10mb'}))
 
 import usersRouter from './routes/users'
 import skinsRouter from './routes/skins'
