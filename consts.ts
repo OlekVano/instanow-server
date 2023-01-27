720203839d9bbbe5b9370bd87857602b1ed2b903
OlekVano <olekvano@gmail.com>
2023-01-27 09:42:03 +0000

    Added a const postKeys in consts.ts

--- consts.ts
+++ consts.ts
@@ -1,10 +1,17 @@
-import { User } from './types'
+import { Post, User } from './types'
 
 export const userKeys: (keyof User)[] = [
-    'nickname',
-    'description',
-    'skin',
-    'background'
+  'nickname',
+  'description',
+  'skin',
+  'background'
 ]
+
+export const postKeys: (keyof Post)[] = [
+  'title',
+  'image',
+  'content'
+]
+
 //https://firebasestorage.googleapis.com/v0/b/{**BUCKET**}/o/{**PATH**}?alt=media
 export const storageBucketPath = 'https://firebasestorage.googleapis.com/v0/b/minecodia.appspot.com'
